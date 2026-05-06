851749dec9fccdf1370d865b97a13808e74d4209
tomoya72457 <tomoya20040507@gmail.com>
2026-05-06 13:04:36 +0900

    Implement basic Flask Todo application with routes for viewing, adding, editing, updating, and deleting todos.

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+#仮のTodoリストを作成
+todos = []
+
+#Todoリストの画面を表示
+@app.route('/')
+def index():
+    return render_template('index.html', todos=todos)
+
+#追加されたTodoをTodoリストに加える
+@app.route('/add', method=['POST'])
+def edit_todo(to_do):
+    todo = request.form.get('todo')
+    todos.append(todo)
+    return redirect(url_for('index'))
+
+#Todoの編集画面を表示
+@app.route('/edit/<int:todo_id>')
+def edit_todo(todo_id):
+    #todoがあるときは編集画面に、そうでない時はTodoリストへ画面遷移する
+    if 1 <= todo_id <= len(todos):
+        return render_template('edit.html', todo=todos[todo_id - 1], todo_id = todo_id)
+    else :
+        return redirect(url_for('index'))
+
+#Todoが更新された時の処理
+@app.route('/update/<int:todo_id>', methods=['POST'])
+def update_todo(todo_id):
+    #変更されたTodoがある場合はTodoリストに追加する
+    if 1 <= todo_id <= len(todos):
+        return render_template('edit.html', todos=todos[todo_id - 1], todo_id=todo_id)
+    else:
+        return redirect(url_for('index'))
+
+#Todoが削除されたときの処理です
+@app.route('/delete/<int:todo_id>')
+def delete_todo(todo_id):
+    #Todoがあるときは削除する
+    if 1 <= todo_id <= len(todos):
+        del todos[todo_id - 1]
+    return redirect(url_for('index'))
+
+#アプリを実行する処理です
+if __name__ == '__main__':
+    app.run(debug=True)
